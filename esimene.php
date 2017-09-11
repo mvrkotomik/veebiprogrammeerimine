5843129c921db47b0b5671e8e471f860d0b51678
Marko Tomik <mvrkotomik@users.noreply.github.com>
2017-09-11 10:42:12 +0300

    lisatud esimene php

---
+++ esimene.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Marko Tomiku veebiprogrammeerimise asjad
+	</title>
+</head>
+<body>
+	<h1>Marko Tomik</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	
+	<?php
+		echo "<p>Täna on vastik ilm!</p>";
+	?>
+	
+</body>
+</html>
