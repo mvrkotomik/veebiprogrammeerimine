f8bb19018e8c400695f1f68636c7518e80ec44d8
Marko Tomik <mvrkotomik@users.noreply.github.com>
2017-09-20 13:26:10 +0300

    Lisatud login.php

---
+++ tund3/login.php
@@ -0,0 +1,24 @@
+<html>
+<label>Logi sisse</label>
+	<br><br>
+	<input name="loginEmail" placeholder="E-post" type="email">
+	<input name="loginPassword" placeholder="Parool" type="password">
+	<br><br>
+	<input type="submit" value="Logi sisse">
+	<br><br>
+	
+<label>Loo kasutaja</label>
+<br><br>
+	<input name="signupFirstName" placeholder="Eesnimi" type="text">
+	<input name="signupFamilyName" placeholder="Perekonnanimi" type="text">
+<br><br>
+<label>Mees</label>
+	<input type="radio" name="gender" value="1">
+<label>Naine</label>
+	<input type="radio" name="gender" value="2">
+<br><br>
+	<input name="signupEmail" placeholder="E-post" tyle="email">
+	<input name="signupPassword" placeholder="Parool" type="password">
+<br><br>
+<input type="submit" value="Loo kasutaja">
+</html>
